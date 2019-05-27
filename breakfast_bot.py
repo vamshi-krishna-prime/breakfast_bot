24842bb36926ef0c65a4583dca53906c9e9feec0
vamshi krishna prime <vamshi.krishna.prime@gmail.com>
2019-05-27 22:58:12 +0530

    Update breakfast_bot.py

--- breakfast_bot.py
+++ breakfast_bot.py
@@ -1,18 +1,21 @@
 import time
-options= ["waffles", "pancakes", "yes", "no"]
+
 def print_pause(message_to_print):
     print(message_to_print)
     time.sleep(2)
 
-def valid_input(prompt, options):
+def valid_input(prompt, option1, option2):
     while True:
-        response= input(prompt).lower()
-        for option in options:
-            if option in response:
-                return response
-        print_pause("Sorry, I don't understand")
+        response = input(prompt).lower()
+        if option1 in response:
+            break
+        elif option2 in response:
+            break
+        else:
+            print_pause("Sorry, I don't understand.")
+    return response
 
-def Intro():
+def intro():
     print_pause("Hello! I am Bob, the Breakfast Bot.")
     print_pause("Today we have two breakfasts available.")
     print_pause("The first is waffles with strawberries and whipped cream.")
@@ -21,24 +24,28 @@ def Intro():
 def get_order():
     response = valid_input("Please place your order. "
                            "Would you like waffles or pancakes?\n",
-                           options)
+                           "waffles", "pancakes")
     if "waffles" in response:
         print_pause("Waffles it is!")
     elif "pancakes" in response:
         print_pause("Pancakes it is!")
     print_pause("Your order will be ready shortly.")
+    order_again()
 
 def order_again():
-    order_again = valid_input("Would you like to place another order? "
+    response = valid_input("Would you like to place another order? "
                               "Please say 'yes' or 'no'.\n",
-                              options)
-    if "no" in order_again:
+                              "yes", "no")
+    if "no" in response:
         print_pause("OK, goodbye!")
-#         break
-    elif "yes" in order_again:
+    elif "yes" in response:
         print_pause("Very good, I'm happy to take another order.")
         get_order()
 
-Intro()
-get_order()
-order_again()
+def order_breakfast():
+    intro()
+    get_order()
+    order_again()
+
+
+order_breakfast()
